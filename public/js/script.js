a492f0d05fc865319d954e70b4ea4bd37499a8bc
hygerth <hygerth@users.noreply.github.com>
2014-06-29 14:29:46 +0200

    Fixed violations reported by jscs

--- public/js/script.js
+++ public/js/script.js
@@ -1,59 +1,58 @@
-(function($){
+(function($) {
 	var IO = {
-		init: function(){
-			IO.socket = io.connect()
-			IO.connectEvents()
-			IO.connectElements()
+		init: function() {
+			IO.socket = io.connect();
+			IO.connectEvents();
+			IO.connectElements();
 		},
-		
-		
-		connectEvents: function(){
-			IO.socket.on('list', function(data){
-				IO.listAllDevices(data)
-			})
+
+		connectEvents: function() {
+			IO.socket.on('list', function(data) {
+				IO.listAllDevices(data);
+			});
 		},
-		
-		connectElements: function(){
-			$(document).on('click', '.switch', IO.sendToogle)
+
+		connectElements: function() {
+			$(document).on('click', '.switch', IO.sendToogle);
 		},
-		
-		listAllDevices: function(data){
-			$('#list').html('')
-			var devices = data.devices
+
+		listAllDevices: function(data) {
+			$('#list').html('');
+			var devices = data.devices;
 			for (var i = 0; i < devices.length; i++) {
-				var device = devices[i]
-				var name = document.createElement('li')
-				name.id = 'name'
-				name.innerHTML = IO.capitalize(device.name)
-				
+				var device = devices[i];
+				var name = document.createElement('li');
+				name.id = 'name';
+				name.innerHTML = IO.capitalize(device.name);
+
 				var status = document.createElement('li')
-				status.innerHTML = '<button id="' + device.status.toLowerCase() + '" class="switch" value="' + device.id + '">' + device.status + '</button>'
-				
-				var row = document.createElement('ul')
-				row.id = 'row'
-				row.appendChild(name)
-				row.appendChild(status)
-				
-				var rowContainer = document.createElement('li')
-				rowContainer.appendChild(row)
-				
-				$('#list').append(rowContainer)
+				status.innerHTML = '<button id="' + device.status.toLowerCase() + '" class="switch" value="' + device.id + '">' + device.status + '</button>';
+
+				var row = document.createElement('ul');
+				row.id = 'row';
+				row.appendChild(name);
+				row.appendChild(status);
+
+				var rowContainer = document.createElement('li');
+				rowContainer.appendChild(row);
+
+				$('#list').append(rowContainer);
 			}
 		},
-		
-		sendToogle: function(){
-			var id = $(this).val()
-			var status = $(this).attr('id')
+
+		sendToogle: function() {
+			var id = $(this).val();
+			var status = $(this).attr('id');
 			var data = {
 				id: id,
 				status: status
 			}
-			IO.socket.emit('toogle', data)
+			IO.socket.emit('toogle', data);
 		},
-		
-		capitalize: function(string){
-			return string.charAt(0).toUpperCase() + string.slice(1)
+
+		capitalize: function(string) {
+			return string.charAt(0).toUpperCase() + string.slice(1);
 		}
 	}
-	IO.init()
-}($))
+	IO.init();
+}($));
--- server.js
+++ server.js
@@ -1,63 +1,59 @@
-var express = require('express'),
-	jade 	= require('jade'),
-	http	= require('http'),
-	port 	= process.env.PORT || 7337,
-	app		= express(),
-	server	= http.createServer(app),
-	io		= require('socket.io').listen(server),
-	exec 	= require('child_process').exec,
-	devices	= []
-	
+var express = require('express');
+var jade 	= require('jade');
+var http	= require('http');
+var port 	= process.env.PORT || 7337;
+var app		= express();
+var server	= http.createServer(app);
+var io		= require('socket.io').listen(server);
+var exec 	= require('child_process').exec;
+var devices	= [];
 
-//app.use(express.logger('dev'))
-app.use(express.bodyParser())
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.static(__dirname + '/public'))
+//app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 
-
-io.sockets.on('connection', function(socket){
-	io.sockets.emit('list', { devices: devices})
-	socket.on('toogle', function(data){
-		var status = 'on'
-		if(data.status == 'on'){
-			status = 'off'
+io.sockets.on('connection', function(socket) {
+	io.sockets.emit('list', {devices: devices});
+	socket.on('toogle', function(data) {
+		var status = 'on';
+		if (data.status == 'on') {
+			status = 'off';
 		}
 		console.log(status);
-		exec('tdtool --' + status + ' ' + parseInt(data.id), function(error, stdout, stderr){})
-		update()
-		io.sockets.emit('list', {devices: devices})
+		exec('tdtool --' + status + ' ' + parseInt(data.id), function(error, stdout, stderr) {});
+		update();
+		io.sockets.emit('list', {devices: devices});
 	})
 })
 
-server.listen(port)
+server.listen(port);
 
-app.get('/', function(req, res){
-
-	res.render('index.jade')
-
+app.get('/', function(req, res) {
+	res.render('index.jade');
 })
 
-//console.log('Starts to listen on port ' + port)
+//console.log('Starts to listen on port ' + port);
 
-var timer = setInterval(function(){
-	update()
-}, 60000)
+var timer = setInterval(function() {
+	update();
+}, 60000);
 
-function update(){
-	exec('tdtool --list', function(error, stdout, stderr){
-		devices = []
-		var devicesString = stdout.replace('\r', '').split('\n')
-		for(i in devicesString){
-			var deviceParams = devicesString[i].split('\t')
-			var params = {}		
-			if(deviceParams.length >= 3){
-				params.id = deviceParams[0]
-				params.name = deviceParams[1]
-				params.status = deviceParams[2]		
-				devices.push(params)
+function update() {
+	exec('tdtool --list', function(error, stdout, stderr) {
+		devices = [];
+		var devicesString = stdout.replace('\r', '').split('\n');
+		for (i in devicesString) {
+			var deviceParams = devicesString[i].split('\t');
+			var params = {};
+			if (deviceParams.length >= 3) {
+				params.id = deviceParams[0];
+				params.name = deviceParams[1];
+				params.status = deviceParams[2];
+				devices.push(params);
 			}
 		}
-		io.sockets.emit('list', { devices: devices})
-	})
+		io.sockets.emit('list', {devices: devices});
+	});
 }
