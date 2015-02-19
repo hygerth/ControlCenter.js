9d1db6ba14961dde2e67a5cd5f7cdabd5f74f3ed
hygerth <hygerth@users.noreply.github.com>
2015-02-19 14:11:48 +0100

    Replaced tabs with whitespaces in client-side script

--- public/js/script.js
+++ public/js/script.js
@@ -1,63 +1,62 @@
 (function($) {
-	var IO = {
-		init: function() {
-			IO.socket = io.connect();
-			IO.connectEvents();
-			IO.connectElements();
-		},
+    var IO = {
+        init: function() {
+            IO.socket = io.connect();
+            IO.connectEvents();
+            IO.connectElements();
+        },
 
-		connectEvents: function() {
-			IO.socket.on('list', function(data) {
-				IO.listAllDevices(data);
-			});
-		},
+        connectEvents: function() {
+            IO.socket.on('list', function(data) {
+                IO.listAllDevices(data);
+            });
+        },
 
-		connectElements: function() {
-			$(document).on('click', 'input', IO.sendToogle);
-		},
+        connectElements: function() {
+            $(document).on('click', 'input', IO.sendToogle);
+        },
 
-		listAllDevices: function(data) {
-			//$('#list').html('');
-			var devices = data.devices || [];
-			var items = $('.list').children();
-			for (var i = 0; i < devices.length; i++) {
-				var device = devices[i];
-				if (i >= items.length) {
-					var li = document.createElement('li');
-					li.className = 'item group';
-					li.innerHTML = '<p></p><label class="toggle"><input type="checkbox"><div class="track"><div class="handle"></div></div></label>';
-					$('.list').append(li);
-					items = $('.list').children();
-				}
-				var item = items[i];
-				$(item).find('p').text(IO.capitalize(device.name));
-				$(item).find('label').attr('id', device.id);
-				var status = IO.convertStatus(device.status);
-				$(item).find('input').prop('checked', status[0]);
-			}
-		},
+        listAllDevices: function(data) {
+            var devices = data.devices || [];
+            var items = $('.list').children();
+            for (var i = 0; i < devices.length; i++) {
+                var device = devices[i];
+                if (i >= items.length) {
+                    var li = document.createElement('li');
+                    li.className = 'item group';
+                    li.innerHTML = '<p></p><label class="toggle"><input type="checkbox"><div class="track"><div class="handle"></div></div></label>';
+                    $('.list').append(li);
+                    items = $('.list').children();
+                }
+                var item = items[i];
+                $(item).find('p').text(IO.capitalize(device.name));
+                $(item).find('label').attr('id', device.id);
+                var status = IO.convertStatus(device.status);
+                $(item).find('input').prop('checked', status[0]);
+            }
+        },
 
-		sendToogle: function() {
-			var id = $(this).parent().attr('id');
-			var status = IO.convertStatus(this.checked);
-			var data = {
-				id: id,
-				status: status[1]
-			};
-			IO.socket.emit('toogle', data);
-		},
+        sendToogle: function() {
+            var id = $(this).parent().attr('id');
+            var status = IO.convertStatus(this.checked);
+            var data = {
+                id: id,
+                status: status[1]
+            };
+            IO.socket.emit('toogle', data);
+        },
 
-		convertStatus: function(status) {
-			if (status === true || status.toString().toLowerCase() === 'on') {
-				return [true, 'on'];
-			} else {
-				return [false, 'off'];
-			}
-		},
+        convertStatus: function(status) {
+            if (status === true || status.toString().toLowerCase() === 'on') {
+                return [true, 'on'];
+            } else {
+                return [false, 'off'];
+            }
+        },
 
-		capitalize: function(string) {
-			return string.charAt(0).toUpperCase() + string.slice(1);
-		}
-	};
-	IO.init();
+        capitalize: function(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+    };
+    IO.init();
 }($));
